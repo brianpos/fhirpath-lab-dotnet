d96bf67b8b7aec3383312fb6eab8075abbcc4389
Brian Postlethwaite <brian_pos@hotmail.com>
2024-07-25 20:46:35 +1000

    Upadte to Firely SDK 5.9.0

--- fhirpath-lab-dotnet/FunctionFhirPathTest.cs
+++ fhirpath-lab-dotnet/FunctionFhirPathTest.cs
@@ -93,7 +93,7 @@ namespace FhirPathLab_DotNetEngine
 			engine._xmlParser = new r4b.Hl7.Fhir.Serialization.FhirXmlParser().Parse<OperationOutcome>;
 			engine._jsonParser = new r4b.Hl7.Fhir.Serialization.FhirJsonParser().Parse<OperationOutcome>;
 			
-            var resultResource = await engine.RunFhirPathTest(req, _logger, "Firely-5.8.1 (R4B)");
+            var resultResource = await engine.RunFhirPathTest(req, _logger, "Firely-5.9.0 (R4B)");
             resultResource.ResourceBase = new Uri($"{req.Scheme}://{req.Host}/api");
 
             var result = new FhirObjectResult(HttpStatusCode.OK, resultResource);
@@ -113,7 +113,7 @@ namespace FhirPathLab_DotNetEngine
 			engine._xmlParser = new r5.Hl7.Fhir.Serialization.FhirXmlParser().Parse<OperationOutcome>;
 			engine._jsonParser = new r5.Hl7.Fhir.Serialization.FhirJsonParser().Parse<OperationOutcome>;
 			
-            var resultResource = await engine.RunFhirPathTest(req, _logger, "Firely-5.8.1 (R5)");
+            var resultResource = await engine.RunFhirPathTest(req, _logger, "Firely-5.9.0 (R5)");
             resultResource.ResourceBase = new Uri($"{req.Scheme}://{req.Host}/api");
 
             var result = new FhirObjectResult(HttpStatusCode.OK, resultResource);
